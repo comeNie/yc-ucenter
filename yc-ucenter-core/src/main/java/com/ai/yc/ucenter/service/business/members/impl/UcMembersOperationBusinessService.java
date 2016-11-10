c51426f41a11f52003dc744bf6acbd670c5f7340
mb3333mb <mengbo@asiainfo.com>
2016-11-10 20:52:34 +0800

    验证码

--- yc-ucenter-core/src/main/java/com/ai/yc/ucenter/service/business/members/impl/UcMembersOperationBusinessService.java
+++ yc-ucenter-core/src/main/java/com/ai/yc/ucenter/service/business/members/impl/UcMembersOperationBusinessService.java
@@ -3,35 +3,44 @@ package com.ai.yc.ucenter.service.business.members.impl;
 
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ai.opt.sdk.web.model.ResponseData;
 import com.ai.yc.ucenter.api.members.param.UcMembersResponse;
 import com.ai.yc.ucenter.api.members.param.base.ResponseCode;
 import com.ai.yc.ucenter.api.members.param.base.ResponseMessage;
+import com.ai.yc.ucenter.api.members.param.login.UcMembersLoginResponse;
 import com.ai.yc.ucenter.api.members.param.opera.UcMembersActiveRequest;
 import com.ai.yc.ucenter.api.members.param.opera.UcMembersGetOperationcodeRequest;
 import com.ai.yc.ucenter.api.members.param.opera.UcMembersGetOperationcodeResponse;
-
 import com.ai.yc.ucenter.constants.CheckMobilResultCodeConstants;
 import com.ai.yc.ucenter.constants.EditMobileResultCodeConstants;
 import com.ai.yc.ucenter.constants.OperationtypeConstants;
-
+import com.ai.yc.ucenter.constants.ResultCodeConstants;
+import com.ai.yc.ucenter.dao.mapper.bo.UcMembers;
+import com.ai.yc.ucenter.service.atom.members.IUcMembersAtomService;
 import com.ai.yc.ucenter.service.atom.members.IUcMembersOperationAtomService;
 import com.ai.yc.ucenter.service.business.members.IUcMembersOperationBusinessService;
 import com.ai.yc.ucenter.util.OperationValidateUtils;
+import com.ai.yc.ucenter.util.UCDateUtils;
 import com.ai.yc.ucenter.util.UcmembersValidators;
 
 
 
 @Component
 @Transactional
-public class UcMembersOperationBusinessService implements IUcMembersOperationBusinessService {
+public class UcMembersOperationBusinessService extends UcMembersBusinessService implements IUcMembersOperationBusinessService {
 	@Autowired
 	private IUcMembersOperationAtomService iUcMembersOperationAtomService;
+	
+	@Autowired
+	private IUcMembersAtomService iUcMembersAtomService;
 	@Override
 	public UcMembersGetOperationcodeResponse saveOperationcode(UcMembersGetOperationcodeRequest request) {
 		UcMembersGetOperationcodeResponse response = new UcMembersGetOperationcodeResponse();
@@ -43,50 +52,56 @@ public class UcMembersOperationBusinessService implements IUcMembersOperationBus
 		if(OperationtypeConstants.EMAIL_ACTIV.equals(operationtype) || OperationtypeConstants.MOBILE_VALI.equals(operationtype)
 				|| OperationtypeConstants.EMAIL_VALI.equals(operationtype)){
 			if(!StringUtils.isNotBlank(request.getUid()+"")){
-				ResponseMessage responseMessage = new ResponseMessage(true, CheckMobilResultCodeConstants.FAIL_CODE, "失败");
-				ResponseCode responseCode = new ResponseCode(CheckMobilResultCodeConstants.EXIST_ERROR, "Uid不能为空");	
-			
-				response.setCode(responseCode);
-				response.setMessage(responseMessage);
-				
+
+				response = (UcMembersGetOperationcodeResponse) addResponse(response,true,CheckMobilResultCodeConstants.EXIST_ERROR, "Uid不能为空", null);
 				return response;
 			}
 		}
-		
+		String responseUid = "";
 		//1、生成手机激活码操作
 		if(OperationtypeConstants.MOBILE_ACTIV.equals(operationtype)){
 			//首先判断手机号是否被注册的合法性，如果注册过直接返回获取失败
 			if(!UcmembersValidators.validateMobilephone(request.getUserinfo())){
-				ResponseMessage responseMessage = new ResponseMessage(true, CheckMobilResultCodeConstants.FAIL_CODE, "失败");
-				ResponseCode responseCode = new ResponseCode(CheckMobilResultCodeConstants.EXIST_ERROR, "该手机号已被注册");	
-			
-				response.setCode(responseCode);
-				response.setMessage(responseMessage);
-				
+				response = (UcMembersGetOperationcodeResponse) addResponse(response,true,CheckMobilResultCodeConstants.EXIST_ERROR, "该手机号已被注册", null);
 				return response;
 			}
 			
 			//如果没有注册过，需根据手机号注册账号，账号状态为未激活，生成并返回手机激活码		
+			else{
+				UcMembers mobileUcmebers = new UcMembers();
+				mobileUcmebers.setMobilephone(request.getUserinfo());
+				mobileUcmebers.setUsername(request.getUserinfo());
+				mobileUcmebers.setEnablestatus("0");
+				
+				mobileUcmebers.setEmailcheck(0); 
+				Integer regdate =(int)UCDateUtils.getSystime() ;
+				mobileUcmebers.setRegdate(regdate);
+				mobileUcmebers.setLastloginip("0");
+				
+				mobileUcmebers.setLogincount(0);
+				mobileUcmebers.setModifydate(0);
+			    responseUid = iUcMembersAtomService.insertMemberPo(mobileUcmebers);
 		
+
+			
+			}
 		}//2、手机动态密码
 		else if(OperationtypeConstants.DYN_PASS.equals(operationtype)){
 			//首先判断手机号是否被注册的合法性，如果注册过直接返回获取失败
 			if(!UcmembersValidators.validateMobilephone(request.getUserinfo())){
-				ResponseMessage responseMessage = new ResponseMessage(true, CheckMobilResultCodeConstants.FAIL_CODE, "失败");
-				ResponseCode responseCode = new ResponseCode(CheckMobilResultCodeConstants.EXIST_ERROR, "该手机号已被注册");	
-				response.setCode(responseCode);
-				response.setMessage(responseMessage);
+				response = (UcMembersGetOperationcodeResponse) addResponse(response,true,CheckMobilResultCodeConstants.EXIST_ERROR, "该手机号已被注册", null);
 				return response;
 			}
 		}
 
 		String operationcode =  iUcMembersOperationAtomService.saveOperationcode(request);
 		if(StringUtils.isNotBlank(operationcode)){
-			ResponseMessage responseMessage = new ResponseMessage(true, CheckMobilResultCodeConstants.SUCCESS_CODE, "成功");
-			ResponseCode responseCode = new ResponseCode(CheckMobilResultCodeConstants.SUCCESS_CODE, "成功");	
-			response.setOperationcode(operationcode);
-			response.setCode(responseCode);
-			response.setMessage(responseMessage);
+			
+			
+			Map<Object, Object> responseDate = new HashMap<Object, Object>();
+			responseDate.put("uid", responseUid);
+			responseDate.put("operationcode",operationcode);
+			response = (UcMembersGetOperationcodeResponse) addResponse(response,true,CheckMobilResultCodeConstants.SUCCESS_CODE, "成功", responseDate);
 		}
 		return response;
 	}
@@ -97,31 +112,35 @@ public class UcMembersOperationBusinessService implements IUcMembersOperationBus
 		UcMembersResponse response = new UcMembersResponse();
 		String operationtype = request.getOperationtype();
 		String operationcode = request.getOperationcode();
+		
+		List<String > listValidator  = beanValidator(request);
+		if(listValidator != null&&!listValidator.isEmpty()){
+			response = (UcMembersResponse) addResponse(response,true,ResultCodeConstants.ERROR_CODE, listValidator+"", null);
+			return response;
+		}
 		//判断激活码
 		if(OperationtypeConstants.EMAIL_ACTIV.equals(operationtype)
 				||OperationtypeConstants.MOBILE_ACTIV.equals(operationtype)){
 			if(!OperationValidateUtils.mobileActivAndDyan(request.getUid(), operationcode)){
-				ResponseMessage responseMessage = new ResponseMessage(true, EditMobileResultCodeConstants.FAIL_CODE, "失败");
-				ResponseCode responseCode = new ResponseCode(EditMobileResultCodeConstants.OVERDUE_ERROR, "验证码过期");	
-				response.setCode(responseCode);
-				response.setMessage(responseMessage);
+	
+				response = (UcMembersResponse) addResponse(response,true,EditMobileResultCodeConstants.OVERDUE_ERROR, "验证码过期", null);
+				return response;
 			}
 			
 			int resultActive =   iUcMembersOperationAtomService.updateActiveMember(request);
 			if(resultActive>0){
-				ResponseMessage responseMessage = new ResponseMessage(true, EditMobileResultCodeConstants.SUCCESS_CODE, "成功");
-				ResponseCode responseCode = new ResponseCode(EditMobileResultCodeConstants.SUCCESS_CODE, "激活成功");	
-				response.setCode(responseCode);
-				response.setMessage(responseMessage);
+				
+				response = (UcMembersResponse) addResponse(response,true,EditMobileResultCodeConstants.SUCCESS_CODE, "成功", null);
 			}
 			
 		}//判断验证码
 		else{
 			if(!OperationValidateUtils.emailVali(request.getUid(), operationcode)){
-				ResponseMessage responseMessage = new ResponseMessage(true, EditMobileResultCodeConstants.FAIL_CODE, "失败");
-				ResponseCode responseCode = new ResponseCode(EditMobileResultCodeConstants.OVERDUE_ERROR, "验证码过期");	
-				response.setCode(responseCode);
-				response.setMessage(responseMessage);
+
+				response = (UcMembersResponse) addResponse(response,true,EditMobileResultCodeConstants.OVERDUE_ERROR, "验证码过期", null);
+				
+			}else{
+				response = (UcMembersResponse) addResponse(response,true,EditMobileResultCodeConstants.SUCCESS_CODE, "成功", null);
 			}
 		}
 		return response;
